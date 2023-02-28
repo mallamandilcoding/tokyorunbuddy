01729167ddc8eeee8198ae1cf2f433b7ca39995b
Emika Sangu <sanguemika@yahoo.com>
2023-02-28 11:58:03 +0900

    created user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database. Removing all existing users and runs..."
+Run.destroy_all
+User.destroy_all
+
+puts "Creating users..."
+User.create!(
+  first_name: "Mandil",
+  last_name: "Malla",
+  email: "mallamandilcoding@gmail.com",
+  password: 123123,
+  time_of_day_preference: "morning",
+  temperature_preference: 15,
+)
+User.create!(
+  first_name: "Emika",
+  last_name: "Sangu",
+  email: "em888ka@gmail.com",
+  password: 123123,
+  time_of_day_preference: "afternoon",
+  temperature_preference: 20,
+)
+User.create!(
+  first_name: "Kurisu",
+  last_name: "Sutairusu",
+  email: "sutairusu@me.com",
+  password: 123123,
+  time_of_day_preference: "evening",
+  temperature_preference: 10,
+)
+User.create!(
+  first_name: "John",
+  last_name: "Baker",
+  email: "john.baker@gmail.com",
+  password: 123123,
+  time_of_day_preference: "morning",
+  temperature_preference: 20,
+)
+User.create!(
+  first_name: "Keina",
+  last_name: "Morikawa",
+  email: "keina.morikawa@me.com",
+  password: 123123,
+  time_of_day_preference: "evening",
+  temperature_preference: 15,
+)
