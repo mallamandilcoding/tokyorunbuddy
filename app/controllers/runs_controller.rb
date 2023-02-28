d8f0176d598276e29b90747bdc755145a19cbd04
mandil malla <mallamandilcoding@gmail.com>
2023-02-28 11:27:15 +0900

    made the basic pundit

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -6,17 +6,27 @@ class RunsController < ApplicationController
   end
 
   def index
+    # @bookings = policy_scope(Booking) (eg)
+    @runnings = policy_scope(Run)
   end
 
   def edit
+    # do the logic here
+    authorize @run
   end
 
   def show
+    # do the logic here
+    authorize @run
   end
 
   def update
+    # do the logic here
+    authorize @run
   end
 
   def destroy
+    # do the logic here
+    authorize @run
   end
 end
