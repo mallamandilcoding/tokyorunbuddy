2b20dfe2f56c7bfac4ab9fa9cfcc8900b893d0fb
sutairusu <sutairusu@icloud.com>
2023-03-02 11:49:38 +0900

    Fix another issue with the seeds -- set the user column as optional for the location model.

--- app/models/run.rb
+++ app/models/run.rb
@@ -4,6 +4,5 @@ class Run < ApplicationRecord
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location_id?
   has_many_attached :photos
-  validates :date, :start_time, :end_time, :status, :weather_description, :wind, :humidity, :precipitation, :temperature, :air_quality, :user, presence: true
-
+  validates :date, :start_time, :end_time, :status, :weather_description, :wind, :humidity, :precipitation, :temperature, :air_quality, presence: true
 end
