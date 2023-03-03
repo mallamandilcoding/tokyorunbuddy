46bdc9d04fcc43b8dd655911e9f053f6d0a6ad3c
Emika Sangu <sanguemika@yahoo.com>
2023-03-03 12:20:44 +0900

    fixed read more on location card

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,14 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+// import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+// import HelloController from "./hello_controller"
+// application.register("hello", HelloController)
+
+
+import { Application } from '@hotwired/stimulus'
+import ReadMore from 'stimulus-read-more'
+
+const application = Application.start()
+application.register('read-more', ReadMore)
