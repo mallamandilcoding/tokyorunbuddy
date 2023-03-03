ff399493857400406300365da44c91226bc4fa81
mandil malla <mallamandilcoding@gmail.com>
2023-03-03 11:49:45 +0900

    sign in redirected to suggestions page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,11 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
+  def after_sign_in_path_for(resource)
+    # return the path you want to redirect to after sign in
+    suggestions_path
+  end
+
   private
 
   def skip_pundit?
