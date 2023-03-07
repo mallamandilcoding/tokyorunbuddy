c566359f735f87f788e22b1033d3f14b1ef2ff99
mandil malla <mallamandilcoding@gmail.com>
2023-03-07 11:22:24 +0900

    run page done

--- db/schema.rb
+++ db/schema.rb
@@ -83,6 +83,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_051906) do
     t.string "last_name"
     t.string "time_of_day_preference"
     t.integer "temperature_preference"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
