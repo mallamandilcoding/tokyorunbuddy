f68901bb44b73e2feca3b534ad7c75a101f3a074
mandil malla <mallamandilcoding@gmail.com>
2023-03-10 11:12:44 +0900

    some changes for loading

--- app/javascript/controllers/loader_controller.js
+++ app/javascript/controllers/loader_controller.js
@@ -2,16 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="loader"
 export default class extends Controller {
-  static targets = ["loader"]
+  static targets = ["loader","addbutton","form"]
   connect() {
     console.log("connected from footer navbar");
   }
   showLoader(){
-    console.log('showloader');
+    // console.log('showloader');
+    // console.log(this.addbuttonTarget);
+    // if (this.addbuttonTarget){
+    //   this.addbuttonTarget.classList.add("d-none")
+    // }
     this.loaderTarget.classList.add("show");
-
+    if (this.formTarget) this.formTarget.classList.add("d-none");
     window.addEventListener('load', () => {
+      if (this.formTarget){
+        this.formTarget.classList.remove("d-none");
+        // this.formTarget.style["z-index"] = 1;
+      }
       this.loaderTarget.classList.remove("show");
     })
   }
+
+
 }
